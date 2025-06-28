b7b3563ade5cb308ab534f9bd08439a68282727f
Mohan <mohanshingare2018@gmail.com>
2025-06-28 15:26:09 +0530

    Project addded

---
+++ SupplierProject/src/main/java/com/Supplier/SupplierProject/SupplierProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Supplier.SupplierProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierProjectApplication.class, args);
+	}
+
+}
---
+++ SupplierProject/src/test/java/com/Supplier/SupplierProject/SupplierProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Supplier.SupplierProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
